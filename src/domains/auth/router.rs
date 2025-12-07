67c77aaa5f667e23809a0b578ce45ff719d0efde
Okpainmo <okpainmoandrew@gmail.com>
2025-12-07 17:21:26 +0100

    chore: implemented the logout end-point, cookie handling, and more

--- src/domains/auth/router.rs
+++ src/domains/auth/router.rs
@@ -2,10 +2,13 @@ use axum::{Router, routing::post, Extension};
 use crate::domains::auth::controllers::login_user::login_user;
 use crate::domains::auth::controllers::register_user::register_user;
 use super::controllers::{login_user, register_user};
+use tower_cookies::{Cookie, CookieManagerLayer, Cookies};
+use crate::domains::auth::controllers::logout_user::logout_user;
 
 pub fn auth_routes() -> Router {
     Router::new()
-        .route("/auth/login", post(login_user))
         .route("/auth/register", post(register_user))
-        // .layer(Extension(db_pool));
+        .route("/auth/login", post(login_user))
+        .route("/auth/logout", post(logout_user))
+        .layer(CookieManagerLayer::new())
 }
