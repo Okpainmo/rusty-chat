b7b7aa3efd89a85d7b6204e818d3b8b7907cd3b1
Okpainmo <okpainmoandrew@gmail.com>
2025-12-20 13:14:39 +0100

    chore: trashed content

--- trash/mod.rs
+++ trash/mod.rs
@@ -1,2 +1,3 @@
 mod lib;
 mod s3_upload_via_middleware_sample_file;
+pub mod random_code_generator;
---
+++ trash/random_code_generator.rs
@@ -0,0 +1,9 @@
+
+struct CodeSpecs {
+    code: String,
+}
+
+pub fn random_code_generator(digits: i64) -> CodeSpecs {
+    let n_stater = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let c_starter: Vec<char> = ('a'..='z').collect();
+}
